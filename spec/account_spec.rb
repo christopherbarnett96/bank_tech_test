558fa7cb9ea86e53dd2d71cc56f1086b3d64d731
Topher <topherb99@gmail.com>
2019-12-17 13:54:37 +0000

    user can debit account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,10 +3,23 @@ require 'account'
 describe Account do
 
   context 'Balance' do
+
     it 'shows the correct balance' do
       subject.add(10)
       expect(subject.balance).to eq 10.00
     end
+  end
+  context 'Debiting account' do
+
+    before do
+      subject.add(20.00)
+    end
+
+    it 'User can withdraw' do
+      subject.withdraw(10.00)
+      expect(subject.balance).to eq 10.00
+    end
+
   end
 
 end
